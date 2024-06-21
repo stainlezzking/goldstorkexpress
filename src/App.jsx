31577b22bc4714e0cfa4ff7ec1db0f5ccc7048b9
Chukwuebuka Stanley <azukachukwuebuka07@gmail.com>
2024-06-21 13:36:54 +0100

    set up application with react and shadcn ui

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-2xl"> Transport Application</h1>
+      <Button> My First Component</Button>
+    </>
+  );
+}
+
+export default App;
