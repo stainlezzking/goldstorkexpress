e457f2e3cce87571d5a3e7f05556ff7f7ec6788f
Chukwuebuka Stanley <azukachukwuebuka07@gmail.com>
2024-07-15 22:38:40 +0100

    added all the blogs(3) page

--- src/pages/one-blog.jsx
+++ src/pages/one-blog.jsx
@@ -2,14 +2,44 @@ import Section from "@/components/builders/section.component";
 import banner from "../assets/pages-banner/blog.png";
 import Bannersection from "@/components/builders/banner.component";
 import { useLoaderData } from "react-router-dom";
+import BlogTitleText from "@/components/builders/blog.title.text";
+import Footer from "@/components/builders/footer.component";
 
 export default function Oneblog() {
   const blog = useLoaderData();
-  console.log(blog);
+  console.log(blog.body[2].title, blog.body[2].text);
   return (
     <div>
       <Bannersection topic={blog.title} src={banner} subtitle="Blog" />
-      <Section></Section>
+      <Section>
+        <div className="space-y-4">
+          {blog.body.map((content, index) => {
+            if (content.sub) {
+              return (
+                <div className="space-y-3" key={index}>
+                  <div className="space-y-2">
+                    <h1 className="font-bold">{content.title}</h1>
+                    <p className="text-[#666C89] font-medium">{content.text}</p>
+                  </div>
+                  {content.sub.map((ct, i) => (
+                    <div className="space-y-2">
+                      <h1 className="font-medium text-sm ">{ct.title}</h1>
+                      <p className="text-[#666C89] font-medium text-sm">{ct.text}</p>
+                    </div>
+                  ))}
+                </div>
+              );
+            }
+            return (
+              <div className="space-y-2">
+                <h1 className="font-bold">{content.title}</h1>
+                <p className="text-[#666C89] font-medium">{content.text}</p>
+              </div>
+            );
+          })}
+        </div>
+      </Section>
+      <Footer />
     </div>
   );
 }
